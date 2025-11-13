86db7b952d521cd17d3e57d8102010ce7a79df95
Edmealem.k <edmealemkassahun@gmail.com>
2025-11-14 01:04:05 +0300

    Time: 1 ms (99.00%) | Memory: 73.7 MB (16.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -7,13 +7,13 @@ var productExceptSelf = function(nums) {
     const res = new Array(n).fill(1);
 
     for (let i = 1; i < n; i++) {
-        res[i] = res[i - 1] * nums[i - 1];
+        res[i] = nums[i-1] * res[i-1]
     }
 
     let postfix = 1;
     for (let i = n - 1; i >= 0; i--) {
-        res[i] *= postfix;
-        postfix *= nums[i]
+        res[i] = postfix * res[i]
+        postfix = postfix * nums[i]
     }
 
     return res
