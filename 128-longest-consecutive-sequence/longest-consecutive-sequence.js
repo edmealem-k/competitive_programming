a1daf08d22bd1d151e745bcab720e410fbd837b5
Edmealem.k <edmealemkassahun@gmail.com>
2025-11-15 00:27:02 +0300

    Time: 61 ms (14.50%) | Memory: 84.9 MB (11.14%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,6 +3,20 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+    // const numSet = new Set(nums);
+    // let longest = 0;
+
+    // for (const n of nums) {
+    //     if (!numSet.has(n - 1)) {
+    //         let length = 0;
+    //         while (numSet.has(n + length)) {
+    //             length += 1;
+    //         }
+    //         longest = Math.max(length, longest)
+    //     }
+    // }
+    // return longest
+    
     const mp = new Map();
     let res = 0;
 
