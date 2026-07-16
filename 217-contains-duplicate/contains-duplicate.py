fd0f2d70baddbdefab12121cbdf2e612a6b5c4da
Edmealem.k <edmealemkassahun@gmail.com>
2026-07-16 20:03:42 +0300

    Time: 11 ms (70.89%) | Memory: 32.2 MB (58.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        hashset = set()
-        for n in nums:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+       hashset = set()
+       for n in nums:
             if n in hashset:
                 return True
             hashset.add(n)
-        return False
+        
+       return False
