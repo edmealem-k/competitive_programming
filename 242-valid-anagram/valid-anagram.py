529809a30ec511b9b4db5a8d57cd767f4340ba1d
Edmealem.k <edmealemkassahun@gmail.com>
2026-07-18 21:08:51 +0300

    Time: 12 ms (55.88%) | Memory: 19.4 MB (75.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        count = {}
-        for char in s:
-            count[char] = count.get(char, 0) + 1
-        for char in t:
-            if char not in count or count[char] < 1:
+            
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
                 return False
-            count[char] -= 1
+        
         return True
