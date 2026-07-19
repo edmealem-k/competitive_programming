19b5c504f84a344db5d5bf65c6df7919de7a80eb
Edmealem.k <edmealemkassahun@gmail.com>
2026-07-19 07:13:29 +0300

    Time: 1 ms (59.73%) | Memory: 57.5 MB (7.83%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+   let res = ""
+
+   for (let i = 0; i < strs[0].length; i++) {
+        for (const str of strs) {
+            if (i === str.length || strs[0][i] !== str[i]) {
+                return res
+            }
+        }
+
+        res += strs[0][i] 
+   } 
+
+   return res
+}; 
