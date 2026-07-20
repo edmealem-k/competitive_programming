2d51d638924f9735ca1ce61afcd46595e7191a76
Edmealem.k <edmealemkassahun@gmail.com>
2026-07-20 12:39:07 +0300

    Time: 34 ms (48.01%) | Memory: 67.5 MB (60.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,15 +3,28 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const res = {};
-    for (let s of strs) {
-        const count = new Array(26).fill(0);
-        for (let c of s) {
-            count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
-        }
-        const key = count.join();
-        if (!res[key]) res[key] = [];
-        res[key].push(s)
+   const res = {};
+
+   for (const s of strs) {
+    // create an array of 26 zeros
+    const count = new Array(26).fill(0);
+
+    // count frequencies using charCodeAt
+    for (const c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
-    return Object.values(res);
+
+    // convert array to string keys (e.g. "1,0,0,1...")
+    const key = count.join(',');
+
+    // intialize the group if it does not exist
+    if (!res[key]){
+        res[key] = [];
+    }
+
+    res[key].push(s);
+   } 
+
+   // return all grouped anagram arrays
+   return Object.values(res);
 };
