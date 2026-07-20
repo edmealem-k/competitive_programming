2d3341538a235f3a1bc9dee440333d1fc45cd376
Edmealem.k <edmealemkassahun@gmail.com>
2026-07-20 12:56:23 +0300

    Time: 13 ms (27.06%) | Memory: 11.3 MB (14.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,15 +1,21 @@
 func groupAnagrams(strs []string) [][]string {
     res := make(map[[26]int][]string)
+
     for _, s := range strs {
         var count [26]int
+
         for _, c := range s {
             count[c - 'a']++
         }
-        res[count] = append(res[count],s)
+
+        res[count] = append(res[count], s)
     }
-    var result [][]string
+
+    result := make([][]string, 0, len(res))
+
     for _, group := range res {
         result = append(result, group)
     }
+
     return result
 }
