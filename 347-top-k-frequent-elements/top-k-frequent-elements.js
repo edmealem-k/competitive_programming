093ef3cecc65ef81212b6cd32edebdf099786e0e
Edmealem.k <edmealemkassahun@gmail.com>
2026-08-09 23:22:32 +0300

    Time: 15 ms (45.01%) | Memory: 66.9 MB (5.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -1,21 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
 var topKFrequent = function(nums, k) {
-    const freq = Array.from({length: nums.length + 1}, () => [])
-    const count = {};
+    const count = new Map();
 
+    // Initialize bucket array with empty sub-arrays (length = nums.length + 1)
+    const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+    // 1. Populate frequency of each number
     for (const n of nums) {
-        count[n] = (count[n] || 0) + 1;
+        count.set(n, (count.get(n) || 0) + 1);
     }
-    for (const n in count) {
-        freq[count[n]].push(parseInt(n));
-    }      
 
+    // 2. Populate frequency buckets
+    for (const [n, c] of count.entries()) {
+        freq[c].push(n);
+    }
+
+    // 3. Iterate backwards from highest frequency bucket to collect top k elements
     const res = [];
     for (let i = freq.length - 1; i>0; i--) {
         for (const n of freq[i]) {
-            res.push(n)
+            res.push(n);
             if (res.length === k) {
                 return res;
             }
         }
-    }    
+    }
+
+    return res;
 };
