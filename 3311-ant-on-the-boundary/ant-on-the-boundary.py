eadebbb802ef305a60d56b46d699a9f3ccc081c2
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-01 00:43:05 +0300

    Time: 43 ms (96.18%) | Memory: 19.2 MB (47.71%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.py
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.py
@@ -2,8 +2,7 @@ class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
         steps = 0
         count = 0
-        # 2,3,-5
-        # num 2, step
+        
         for i in range(len(nums)):
             steps += nums[i]
             if steps == 0:
