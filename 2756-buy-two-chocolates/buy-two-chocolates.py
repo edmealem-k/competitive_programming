994652bb05e79913303de9fc25daeb0a1a463a24
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-02 01:26:02 +0300

    Time: 7 ms (5.44%) | Memory: 19.4 MB (19.12%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,8 +1,28 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort()
-        cost = prices[0] + prices[1]
+        # prices 1,3,4,2
+        # money 3
+        # [1, 2] -> leftover = 0 without going -ve
+
+        # prices 3, 2, 3
+        # money 3
+        # we can buy two chocolates without going to dubt, so we wont buy
+        # and return the money
+        leftover = 0
+        bought = False
+
+
+        #  prices = [3,2,3]
+        #  money = 3
+        #  expected output = 0
+        #  i = 2, diff = money - prices[i] = 3 - 3 = 0
+        #  j = 1, diff >= prices[j]
+        for i in range(len(prices)):
+            diff = money - prices[i]
+
+            for j in range(i + 1, len(prices)):
+                if (diff >= prices[j]):
+                    leftover = max(leftover, diff - prices[j])
+                    bought = True
         
-        if cost <= money:
-            return money - cost
-        return money
+        return leftover if bought else money
