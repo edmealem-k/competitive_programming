cf610014a511f382d13216cf32e98a6631f7fc78
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-12 01:10:58 +0300

    Time: 11 ms (22.42%) | Memory: 62.5 MB (79.17%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
@@ -1,3 +1,8 @@
 function sortedSquares(nums: number[]): number[] {
-    return nums.map(num => num ** 2).sort((a,b) => a - b)
+    // return nums.map(num => num ** 2).sort((a,b) => a - b)
+    let na: number[] = [];
+    for (let n of nums) {
+        na.push(n * n)
+    }
+    return na.sort((a, b) => a - b)
 };
