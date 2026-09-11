c0d58d4ff1556525fb501c1596c9661d8cd6bfbb
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-12 02:22:54 +0300

    Time: 2 ms (93.06%) | Memory: 63.4 MB (30.56%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
@@ -1,8 +1,22 @@
 function sortedSquares(nums: number[]): number[] {
     // return nums.map(num => num ** 2).sort((a,b) => a - b)
-    let na: number[] = [];
-    for (let n of nums) {
-        na.push(n * n)
+    let n = nums.length
+    let res:number[] = new Array(n).fill(0);
+    let left = 0;
+    let right = n - 1;
+
+    // [-4,-1,0,3,10]
+    // left = 1; right = 3; res = [0,0,0,16,100]
+    // i = 3
+    for (let i = n - 1; i >= 0; i--) {
+        if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+            res[i] = nums[left] ** 2;
+            left++;
+        } else {
+            res[i] = nums[right] ** 2;
+            right--;
+        }
     }
-    return na.sort((a, b) => a - b)
+
+    return res
 };
