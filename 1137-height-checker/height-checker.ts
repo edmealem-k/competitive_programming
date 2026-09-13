ca599efca5c5f887661aade56ed6bce0662ffc15
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-13 22:01:58 +0300

    Time: 3 ms (14.08%) | Memory: 55.7 MB (40.84%) - LeetSync

--- 1137-height-checker/height-checker.ts
+++ 1137-height-checker/height-checker.ts
@@ -7,14 +7,22 @@ function heightChecker(heights: number[]): number {
 
     let idx = 0;
     let output = 0; 
+    let expected: number[] = []
 
     for (let i = 0; i < count.length; i++) {
         while(count[i] != 0) {
-            if (heights[idx] != i) {
-                output++
-            }
+            // if (heights[idx] != i) {
+            //     output++
+            // }
+            expected.push(i)
             count[i]--;
             idx++;
+        }
+    }
+
+    for (let i = 0; i < heights.length; i++) {
+        if (expected[i] !== heights[i]) {
+            output++
         }
     }
 
