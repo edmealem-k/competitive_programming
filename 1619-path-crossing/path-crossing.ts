833b60a8e793cb502cd021d29fd9c1125a76e51b
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-16 21:26:38 +0300

    Time: 0 ms (100.00%) | Memory: 55.8 MB (43.48%) - LeetSync

--- 1619-path-crossing/path-crossing.ts
+++ 1619-path-crossing/path-crossing.ts
@@ -1,23 +1,25 @@
 function isPathCrossing(path: string): boolean {
     let visited = new Set<string>()
-    visited.add("0,0")
+    visited.add('0,0')
+
     let x = 0;
     let y = 0;
 
     for (const char of path) {
-        switch(char) {
-            case "N": y++; break;
-            case "S": y--; break;
-            case "E": x++; break;
-            case "W":  x--; break;
+        if (char === "N") {
+            y++;
+        } else if (char === "S") {
+            y--;
+        } else if (char === "E") {
+            x++;
+        } else {
+            x--;
         }
 
-        if (visited.has(`${x},${y}`)) {
-            return true;
-        } else {
-            visited.add(`${x},${y}`)
-        }
+        if (visited.has(`${x},${y}`)) return true;
+        else visited.add(`${x},${y}`);
     }
 
-    return false;
+    return false
 };
+
