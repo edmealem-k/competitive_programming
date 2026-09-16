b4740ed873a395da83734319b28465d86932a4ad
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-16 22:19:52 +0300

    Time: 3 ms (75.00%) | Memory: 56.9 MB (90.63%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.ts
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.ts
@@ -1,21 +1,22 @@
 function numSpecial(mat: number[][]): number {
-    const m = mat.length;
-    const n = mat[0].length;
-    let row: number[] = new Array(m).fill(0);
-    let col: number[] = new Array(n).fill(0)
+    let rl = mat.length;
+    let cl = mat[0].length;
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+    let row = new Array(rl).fill(0);
+    let col = new Array(cl).fill(0);
+
+    for (let i = 0; i < rl; i++) {
+        for (let j = 0; j < cl; j++) {
             if (mat[i][j] === 1) {
-                row[i] += 1
-                col[j] += 1
+                row[i]++;
+                col[j]++;
             }
         }
     }
 
     let ans = 0;
-    for (let i = 0; i < m; i++) {
-        for (let j=0; j < n; j++) {
+    for (let i = 0; i < rl; i++) {
+        for (let j = 0; j < cl; j++) {
             if (mat[i][j] === 1 && row[i] === 1 && col[j] === 1) {
                 ans++;
             }
@@ -24,3 +25,53 @@ function numSpecial(mat: number[][]): number {
 
     return ans;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const m = mat.length;
+    // const n = mat[0].length;
+    // let row: number[] = new Array(m).fill(0);
+    // let col: number[] = new Array(n).fill(0)
+
+    // for (let i = 0; i < m; i++) {
+    //     for (let j = 0; j < n; j++) {
+    //         if (mat[i][j] === 1) {
+    //             row[i] += 1
+    //             col[j] += 1
+    //         }
+    //     }
+    // }
+
+    // let ans = 0;
+    // for (let i = 0; i < m; i++) {
+    //     for (let j=0; j < n; j++) {
+    //         if (mat[i][j] === 1 && row[i] === 1 && col[j] === 1) {
+    //             ans++;
+    //         }
+    //     }
+    // }
+
+    // return ans;
