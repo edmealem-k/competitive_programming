8d7458ff3909504a806d2d6146fe987791e05744
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-19 21:15:34 +0300

    Time: 2 ms (0.00%) | Memory: 68.5 MB (100.00%) - LeetSync

--- 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.ts
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.ts
@@ -3,15 +3,13 @@ function findWinningPlayer(skills: number[], k: number): number {
     let currwincount = 0;
     let ptr = 0;
 
-    // [7, 11]
-    // k = 2
     for (let i = 1; i < skills.length; i++) {
         if (skills[ptr] > skills[i]) {
             indx = ptr;
-            currwincount++
+            currwincount++;
         } else {
-            ptr = i;
             indx = i;
+            ptr = i;
             currwincount = 1;
         }
 
@@ -22,3 +20,39 @@ function findWinningPlayer(skills: number[], k: number): number {
 
     return indx;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let indx = 0;
+    // let currwincount = 0;
+    // let ptr = 0;
+
+    // // [7, 11]
+    // // k = 2
+    // for (let i = 1; i < skills.length; i++) {
+    //     if (skills[ptr] > skills[i]) {
+    //         indx = ptr;
+    //         currwincount++
+    //     } else {
+    //         ptr = i;
+    //         indx = i;
+    //         currwincount = 1;
+    //     }
+
+    //     if (currwincount >= k) {
+    //         break;
+    //     }
+    // }
+
+    // return indx;
