aff16f5ec99a09f67188387711024982d39ee139
Edmealem.k <edmealemkassahun@gmail.com>
2026-09-19 20:33:49 +0300

    Time: 64 ms (41.18%) | Memory: 65.6 MB (52.94%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.ts
+++ 3483-alternating-groups-ii/alternating-groups-ii.ts
@@ -1,19 +1,64 @@
 function numberOfAlternatingGroups(colors: number[], k: number): number {
-    let count = 0;
     let n = colors.length;
-    let alternatingLength = 1;
+    let count = 0;
+    let alternativingLength = 1;
 
     for (let i = 1; i < n + k - 1; i++) {
-        if (colors[i % n] !== colors[(i - 1) % n]) {
-            alternatingLength++;
+        if (colors[i % n] !== colors[(i-1) % n]) {
+            alternativingLength++;
         } else {
-            alternatingLength = 1
+            alternativingLength = 1;
         }
 
-        if (alternatingLength >= k) {
-            count++
+        if (alternativingLength >= k) {
+            count++;
         }
     }
 
     return count;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let count = 0;
+    // let n = colors.length;
+    // let alternatingLength = 1;
+
+    // for (let i = 1; i < n + k - 1; i++) {
+    //     if (colors[i % n] !== colors[(i - 1) % n]) {
+    //         alternatingLength++;
+    //     } else {
+    //         alternatingLength = 1
+    //     }
+
+    //     if (alternatingLength >= k) {
+    //         count++
+    //     }
+    // }
+
+    // return count;
